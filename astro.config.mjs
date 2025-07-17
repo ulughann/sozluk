f0466603a5c9d8b4dd3abc29e4fd9b0152e09545
ulughann <ulughann@gmail.com>
2025-07-17 12:07:19 +0300

    Bilgin Sözlük

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
       ],
       sidebar: [
         {
-          label: "Sözlük",
+          label: "Bilgin Sözlük",
           autogenerate: { directory: "sozluk" },
         },
       ],
